defc30214b167e1f7cc20ce418b55bdad7b4e2ac
Eetu Nummelin <e.i.nummelin20@gmail.com>
2023-06-02 15:37:41 +0300

    first task done

--- osio-1/kurssitiedot_step1/src/App.js
+++ osio-1/kurssitiedot_step1/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = (props) => {
+  console.log(props);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{props.courseName}</h1>
+    </>
+
   );
-}
+
+};
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <p>{props.name}, tasks {props.amount}</p>
+
+  );
+};
+
+const Total = (props) => {
+  console.log(props);
+
+  return (
+    <p>
+      Number of exercises {props.amount}
+    </p>
+  );
+
+};
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header courseName={course} />
+      <Content name={part1} amount={exercises1} />
+      <Content name={part2} amount={exercises2} />
+      <Content name={part3} amount={exercises3} />
+      <Total amount={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+};
 
 export default App;
