dded958a22341f50c12b79f4ee29778dd7f35fee
Eetu Nummelin <e.i.nummelin20@gmail.com>
2023-06-19 11:35:01 +0300

    jotakin

--- osio-1/kurssitiedot_step1/src/App.js
+++ osio-1/kurssitiedot_step1/src/App.js
@@ -12,6 +12,7 @@ const Header = (props) => {
 
 const Content = (props) => {
   console.log(props);
+
   return (
     <div>
       <Part name={props.part} amount={props.part1Amount} />
@@ -42,18 +43,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const parts = [
+    {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+];
 
   return (
     <div>
       <Header courseName={course} />
-      <Content part={part1} part1Amount={exercises1} part2={part2} part2Amount={exercises2} part3={part3} part3Amount={exercises3}/>
-      <Total amount={exercises1 + exercises2 + exercises3} />
+      <Content part= {parts}
+      />
+      <Total amount={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 };
