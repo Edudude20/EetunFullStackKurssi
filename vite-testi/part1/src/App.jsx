69b9a12e9f1e4e09f2da5f1c932e933643ade7ac
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-08-30 15:28:29 +0300

    kopioi viteen aiemmin tehdyt tehtävät

--- vite-testi/part1/src/App.jsx
+++ vite-testi/part1/src/App.jsx
@@ -1,36 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
-function App() {
-  const [count, setCount] = useState(0)
+const Header = (props) => {
+  //console.log(props);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{props.course.name}</h1>
+    </>
+
+  );
+
+};
+
+const Content = (props) => {
+  //console.log(props.parts[0].name);
+
+  return (
+    <div>
+      <Part name={props.course.parts[0].name} amount={props.course.parts[0].exercises} />
+      <Part name={props.course.parts[1].name} amount={props.course.parts[1].exercises} />
+      <Part name={props.course.parts[2].name} amount={props.course.parts[2].exercises} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  //console.log(props);
+  return (
+    <p>{props.name}: {props.amount}</p>
+  )
+};
+
+
+const Total = (props) => {
+  //console.log(props.amount.parts);
+
+  let total = 0;
+
+  props.amount.parts.forEach(element => {
+    total += element.exercises;
+  });
+
+  //console.log(total);
+
+  return (
+    <p>
+      Number of exercises total: {total}
+    </p>
+  );
+
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+
+  //const [first, second, third] = parts;
+
+  //console.log(first, second, third);
+
+
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total amount={course} />
     </>
   )
-}
+};
 
-export default App
+export default App;
