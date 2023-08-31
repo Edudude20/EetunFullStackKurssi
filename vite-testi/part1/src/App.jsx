2dede9054962883bb4ed4a4fadfdc042823e3162
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-08-31 15:21:18 +0300

    aloitettu 1c

--- vite-testi/part1/src/App.jsx
+++ vite-testi/part1/src/App.jsx
@@ -1,85 +1,31 @@
-const Header = (props) => {
-  //console.log(props);
+const Hello = (props) => {
+  const bornYear = () => {	
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+
 
   return (
     <>
-      <h1>{props.course.name}</h1>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </>
-
   );
-
-};
-
-const Content = (props) => {
-  //console.log(props.parts[0].name);
-
-  return (
-    <div>
-      <Part name={props.course.parts[0].name} amount={props.course.parts[0].exercises} />
-      <Part name={props.course.parts[1].name} amount={props.course.parts[1].exercises} />
-      <Part name={props.course.parts[2].name} amount={props.course.parts[2].exercises} />
-    </div>
-  );
-};
-
-const Part = (props) => {
-  //console.log(props);
-  return (
-    <p>{props.name}: {props.amount}</p>
-  )
-};
-
-
-const Total = (props) => {
-  //console.log(props.amount.parts);
-
-  let total = 0;
-
-  props.amount.parts.forEach(element => {
-    total += element.exercises;
-  });
-
-  //console.log(total);
-
-  return (
-    <p>
-      Number of exercises total: {total}
-    </p>
-  );
-
 };
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
-
-  //const [first, second, third] = parts;
-
-  //console.log(first, second, third);
-
+  const nimi = 'Pekka';
+  const ika = 10;
 
   return (
     <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total amount={course} />
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
     </>
-  )
+  );
 };
 
 export default App;
