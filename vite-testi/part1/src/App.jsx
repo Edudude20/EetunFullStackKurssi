7593da049fd4a6dd439cca376f665e3ade2d4980
Eetu Nummelin <e.i.nummelin20@gmail.com>
2023-09-02 14:16:46 +0300

    1c kesken

--- vite-testi/part1/src/App.jsx
+++ vite-testi/part1/src/App.jsx
@@ -1,14 +1,18 @@
-const Hello = (props) => {
-  const bornYear = () => {	
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  };
+import { useState } from "react";
 
+const Hello = ({ name, age }) => {
+  //DESTRUKTUROINTI
+  // const name = props.name;
+  // const age = props.age;
+  // const {name,age} = props;
 
+  //console.log(name);
+
+  const bornYear = () => new Date().getFullYear() - age;
   return (
     <>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
       <p>So you were probably born in {bornYear()}</p>
     </>
@@ -16,14 +20,22 @@ const Hello = (props) => {
 };
 
 const App = () => {
-  const nimi = 'Pekka';
+  const nimi = "Pekka";
   const ika = 10;
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
+
+  //console.log('rendering...', counter);
 
   return (
     <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={nimi} age={ika} />
+      <div>{counter}</div>
     </>
   );
 };
