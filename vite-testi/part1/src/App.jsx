e45329874da72cd2556a7bb0b7443a3654b7e64e
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-09-07 12:30:20 +0300

    1d luettu

--- vite-testi/part1/src/App.jsx
+++ vite-testi/part1/src/App.jsx
@@ -20,12 +20,23 @@ const Hello = ({ name, age }) => {
   );
 };
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
+const Display = (props) => {
+  console.log(props);
+  return <div></div>;
 };
 
-const Nappi = ({ handleClick, text }) => {
+const Nappi = (props) => {
+  console.log(props);
+  const { handleClick, text } = props;
   return <button onClick={handleClick}>{text}</button>;
+};
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+
+  return <div>button press history: {props.allClicks.join("-")}</div>;
 };
 
 const App = () => {
@@ -33,10 +44,12 @@ const App = () => {
 
   const nimi = "Pekka";
   const ika = 10;
-  const [counter, setCounter] = useState(0); //useState-hookin avulla luodaan sovellukselle laskurin tilan counter ja asettaa sen tilaksi 0
-  //funktio palauuttaa taulukon jossa on kaksi alkiota. Alkiot otetaan taulukon destrukturointisyntaksilla talteen muuttujiin counter ja setCounter
+  const [counter, setCounter] = useState(0);
+  //useState-hookin avulla luodaan sovellukselle laskurin tilan counter ja asettaa sen tilaksi 0
+  //funktio palauttaa taulukon jossa on kaksi alkiota. Alkiot otetaan taulukon destrukturointisyntaksilla talteen muuttujiin counter ja setCounter
   //Tilaa muuttavan funktion kutsuminen aiheuttaa komponentin uudelleenrenderöitymisen
-  console.log("rendering with counter value", counter);
+
+  //console.log("rendering with counter value", counter);
 
   //TIMER COUNTER
   /*
@@ -65,20 +78,38 @@ const App = () => {
   //#region 1d
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [value, setValue] = useState(10);
+
   /*
   const [clicks, setClicks] = useState({
     left: 0,
     right: 0
   });
+  Kaiken tilan pitäminen yhdessä oliossa on tämän sovelluksen kannalta huono ratkaisu; 
+  etuja siinä ei juuri ole, mutta sovellus monimutkaistuu merkittävästi. 
+  Onkin ehdottomasti parempi ratkaisu tallettaa nappien klikkaukset erillisiin tilan paloihin.
   */
 
   const handleLeftClick = () => {
-    setLeft(left + 1);
+    setAllClicks(allClicks.concat("L"));
+    console.log("left before", left);
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    console.log("left after", updatedLeft);
+    setTotal(updatedLeft + right);
   };
 
   const handleRightClick = () => {
+    setAllClicks(allClicks.concat("R"));
     setRight(right + 1);
+    setTotal(left + right);
   };
+
+const setToValue = (newValue) => {
+  setValue(newValue);
+}
 
   //#endregion
 
@@ -93,10 +124,15 @@ const App = () => {
       <Hello name="Maya" age={26 + 10} />
       <Hello name={nimi} age={ika} />
       <div>
+        <h2>osio 1d</h2>
         {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
+        <Nappi handleClick={handleLeftClick} text='left'/>
+        <Nappi handleClick={handleRightClick} text='right'></Nappi>
         {right}
+        <p>total: {total}</p>
+        <History allClicks={allClicks}></History>
+        <button onClick={() => console.log('clicked the button named button')}>button</button>
+        <Nappi handleClick={() => setToValue(24)} text='set to 24'></Nappi>
       </div>
     </>
   );
