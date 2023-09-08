a3b8aa0a75e465b0daa2a8794bee249cf03c5fcf
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-09-08 16:12:37 +0300

    tehty 1.12 asti

--- osio-1/unicafe/src/App.jsx
+++ osio-1/unicafe/src/App.jsx
@@ -5,25 +5,92 @@ import viteLogo from "/vite.svg";
 import fullStackLogo from "./assets/fullstack.svg";
 import "./App.css";
 
-const Nappi = (props) => {
+const Button = (props) => {
   //console.log(props);
   const { handleClick, text } = props;
   return <button onClick={handleClick}>{text}</button>;
 };
 
+const StatisticLine = ({ text, value }) => {
+  if (text === "positive") {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>{text}</td>
+            <td>{value} %</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const Statistics = (props) => {
+  console.log(props);
+  const { good, bad, neutral, total } = props;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <StatisticLine text="good" value={good}></StatisticLine>
+      <StatisticLine text="neutral" value={neutral}></StatisticLine>
+      <StatisticLine text="bad" value={bad}></StatisticLine>
+      <StatisticLine text="all" value={total}></StatisticLine>
+      <StatisticLine
+        text="average"
+        value={(good - bad) / total}
+      ></StatisticLine>
+      <StatisticLine
+        text="positive"
+        value={(100 * good) / total}
+      ></StatisticLine>
+    </div>
+  );
+};
+
 function App() {
+  //#region MUUTTUJAT
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
 
+  const anectodes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+const [selected, setSelected] = useState(0);
+  //#endregion
+
   const handleGoodClick = () => {
-    console.log("good before", good);
+    //console.log("good before", good);
     const updatedGood = good + 1;
     setGood(updatedGood);
-    console.log("updated good", updatedGood);
+    //console.log("updated good", updatedGood);
     setTotal(total + 1);
-    console.log(total);
+    //console.log("total:", total);
   };
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1;
@@ -37,7 +104,18 @@ function App() {
     setTotal(total + 1);
     console.log(total);
   };
-  
+  const handleRandomSelectedAnectode = () => {
+
+    
+    const min = 0;
+    const max = anectodes.length;
+    //console.log(max);
+    const randomSelection = Math.floor(Math.random() * (max - min)) + min;
+    console.log(randomSelection);
+    setSelected(randomSelection);
+
+  }
+
   return (
     <>
       <div>
@@ -51,18 +129,19 @@ function App() {
       </div>
       <h1>1d Unicafe</h1>
       <h2>Give feedback</h2>
-      <Nappi handleClick={handleGoodClick} text="good"></Nappi>
-      <Nappi handleClick={handleNeutralClick} text="neutral"></Nappi>
-      <Nappi handleClick={handleBadClick} text="bad"></Nappi>
-
-
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average: {(good - bad) / total}</p>
-      <p>positive: {good / total}</p>
+      <Button handleClick={handleGoodClick} text="good 1+"></Button>
+      <Button handleClick={handleNeutralClick} text="neutral 0"></Button>
+      <Button handleClick={handleBadClick} text="bad -1"></Button>
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        total={total}
+      ></Statistics>
+      <Button handleClick={handleRandomSelectedAnectode} text="random anectode"></Button>
+      <div>
+        {anectodes[selected]}
+      </div>
     </>
   );
 }
