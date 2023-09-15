4d030a022fbf38d8b73c66e783da7c10f8f27c69
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-09-15 14:51:26 +0300

    tehtävä 2.6 tehty

--- osio-2/puhelinluettelo/src/App.jsx
+++ osio-2/puhelinluettelo/src/App.jsx
@@ -1,27 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    {
+      name: "Arto Hellas"
+    },
+  ]);
+  const [newName, setNewName] = useState("name");
+
+  const result = persons.map((person) => person.name);
+  console.log(result);
+
+  const handleInputChange = (event) => {
+    //console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name:
+          <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
+};
 
-}
-
-export default App
+export default App;
