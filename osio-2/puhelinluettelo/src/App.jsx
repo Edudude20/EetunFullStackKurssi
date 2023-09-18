366f876cc0177f69a5ddd9b82d098b858a08b201
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-09-18 13:59:55 +0300

    2b kokonaan valmis (paitsi 2.9)

--- osio-2/puhelinluettelo/src/App.jsx
+++ osio-2/puhelinluettelo/src/App.jsx
@@ -1,18 +1,52 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
+
+const PersonForm = (props) => {
+
+  return (
+    <form onSubmit={props.addPerson}>
+      <div>
+        name:
+        <input value={props.newName} onChange={props.handleNameChange} />
+      </div>
+      <div>
+        number:
+        <input value={props.newNumber} onChange={props.handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => {
+  console.log(persons);
+  return (
+    <div>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([
     {
       name: "Arto Hellas",
-      number: "+358 123412312"
-    }
+      number: "+358 123412312",
+    },
   ]);
   const [newName, setNewName] = useState("name");
   const [newNumber, setNewNumber] = useState("+123 456 7890");
 
-
-  const result = persons.map((person) => person.name);
-  //console.log(result);
+  // const result = persons.map((person) => person.name);
+  // //console.log(result);
 
   const handleNameChange = (event) => {
     //console.log(event.target.value);
@@ -25,18 +59,17 @@ const App = () => {
   };
   const addPerson = (event) => {
     event.preventDefault();
-    
-    console.log("new name:", newName);
+
+    //console.log("new name:", newName);
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
     };
-    console.log(persons[0], personObject);
-    console.log(persons.some(element => element.name === newName));
-    if (persons.some(element => element.name === newName)) {
+    // console.log(persons[0], personObject);
+    // console.log(persons.some((element) => element.name === newName));
+    if (persons.some((element) => element.name === newName)) {
       alert(`${newName} is already added to the phonebook`);
       setNewName("");
-
     } else {
       setPersons(persons.concat(personObject));
       setNewName("");
@@ -47,25 +80,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name:
-          <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number:
-          <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}></PersonForm>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name} {person.number}</li>
-        ))}
-      </ul>
+      <Persons persons={persons}></Persons>
     </div>
   );
 };
