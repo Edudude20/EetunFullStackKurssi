eed40cedcc0c3e76824423fc9bbb1a6760b2abba
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-09-18 13:42:01 +0300

    2b valmis

--- osio-2/puhelinluettelo/src/App.jsx
+++ osio-2/puhelinluettelo/src/App.jsx
@@ -3,30 +3,46 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
     {
-      name: "Arto Hellas"
-    },
+      name: "Arto Hellas",
+      number: "+358 123412312"
+    }
   ]);
   const [newName, setNewName] = useState("name");
+  const [newNumber, setNewNumber] = useState("+123 456 7890");
+
 
   const result = persons.map((person) => person.name);
-  console.log(result);
+  //console.log(result);
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     //console.log(event.target.value);
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
   const addPerson = (event) => {
     event.preventDefault();
-    console.log("button clicked", event.target);
-
+    
+    console.log("new name:", newName);
     const personObject = {
-      name: newName
-    }
+      name: newName,
+      number: newNumber
+    };
+    console.log(persons[0], personObject);
+    console.log(persons.some(element => element.name === newName));
+    if (persons.some(element => element.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+      setNewName("");
 
-    setPersons(persons.concat(personObject));
-    setNewName('');
-  }
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
 
   return (
     <div>
@@ -34,7 +50,11 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name:
-          <input value={newName} onChange={handleInputChange} />
+          <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +63,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
